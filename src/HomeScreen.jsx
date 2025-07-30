ecb208f727086c5441cafb96798c3cb45ba5d962
qericho <echstmr@gmail.com>
2025-07-31 00:07:16 +0800

    Initial commit

---
+++ src/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './component/Nav'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <Nav />
+        </>
+    )
+}
+
+export default HomeScreen
---
+++ src/component/Nav.jsx
@@ -0,0 +1,88 @@
+import { FaBars } from "react-icons/fa6";
+import { RxCross1 } from "react-icons/rx";
+import { CiMap } from "react-icons/ci";
+import { useState } from "react";
+
+const Nav = () => {
+    const [openNav, SetOPenNav] = useState(false);
+    const handleNav = () => {
+        SetOPenNav((prev) => !prev);
+        console.log(openNav);
+    };
+
+    return (
+        <nav>
+            {/* Overlay */}
+            {openNav ? "" : (
+                <div className="w-full h-[100vh] absolute left-0 bg-black/50 z-1 md:hidden"></div>
+            )}
+
+            <div className="px-10 py-4 flex items-center justify-between relative">
+                <div className="flex items-center space-x-5 text-sky-700">
+                    <img
+                        className="w-[130px]"
+                        src="https://www.upskwela.com/_astro/logo.BY9msEYF.svg"
+                        alt="Logo"
+                    />
+                    {/* Links */}
+                    <ul className="space-x-5 md:flex hidden">
+                        <li className="text-[15px] font-bold text-sky-500">Home</li>
+                        <li className="text-[15px] font-bold">Roadmap</li>
+                    </ul>
+                </div>
+
+                {/* Login/Sign Up */}
+                <div className="font-bold space-x-3 md:flex hidden">
+                    <button className="px-5 py-2 text-sky-700">Login</button>
+                    <button className="px-5 py-2 bg-sky-500 rounded text-white">Sign Up</button>
+                </div>
+
+                <span
+                    onClick={handleNav}
+                    className="md:hidden flex cursor-pointer"
+                >
+                    <FaBars />
+                </span>
+
+                {/* Mobile Nav */}
+                <div
+                    className={`${openNav ? "right-[-900px]" : "right-0"} md:hidden
+                    h-[100vh] w-[300px] fixed top-0 bg-white z-1`}
+                >
+                    <div className="flex flex-col px-8 py-12 space-y-10 relative">
+                        <ul className="font-bold space-y-5">
+                            <li className="text-sky-500 text-[15px] font-bold bg-black/5 p-2 rounded cursor-pointer">
+                                Home
+                            </li>
+                            <li className="text-[15px] font-bold p-2 rounded flex items-center gap-2 text-sky-700 cursor-pointer">
+                                <CiMap />
+                                Roadmap
+                            </li>
+                        </ul>
+
+                        <div className="w-[85%] h-[1px] bg-[#ddd] absolute top-[150px] left-1/2 -translate-x-1/2"></div>
+
+                        {/* Login/Sign Up */}
+                        <div className="font-bold space-y-5">
+                            <button className="w-full py-2 text-sky-700 cursor-pointer">
+                                Login
+                            </button>
+                            <button className="w-full py-2 text-white bg-sky-500 hover:bg-sky-600 rounded cursor-pointer">
+                                Sign Up
+                            </button>
+                        </div>
+
+                        <span
+                            onClick={handleNav}
+                            className="absolute top-5 right-5 cursor-pointer"
+                        >
+                            <RxCross1 />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
