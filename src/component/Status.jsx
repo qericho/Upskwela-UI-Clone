2276cb4cbe7577b71163a435dc75ffbfed83fddf
qericho <echstmr@gmail.com>
2025-08-01 01:56:00 +0800

    updated

--- src/component/Status.jsx
+++ src/component/Status.jsx
@@ -3,7 +3,7 @@ const Status = () => {
     return (
         <div className="text-center w-full mb-10">
             <h4 className="mb-5 text-sm font-medium uppercase tracking-wider text-[var(--color-secondary)]">PLATFORM STATUS</h4>
-            <ul className="grid grid-cols-4 place-items-center py-4  max-w-[600px]">
+            <ul className="grid grid-cols-4 place-items-center py-4 mx-auto max-w-[600px]">
                 <li className="text-[var(--color-pri)] flex flex-col items-center space-x-3 text-3xl font-bold">
                     Live
                     <span className="text-sm font-normal text-[var(--color-secondary)]">Beta Version</span>
